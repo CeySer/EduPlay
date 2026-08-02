682b84f4e8a3f7943f82bdc69b3a614b28dfc181
Ceyhun Özdemir <ceyhun.oezdemir@outlook.com>
2026-08-03 00:11:32 +0200

    Add files via upload

--- datenbanken.js
+++ datenbanken.js
@@ -16,3 +16,55 @@
 
 // FORMELN
 // Die Variable FORMULA_DATABASE wird in formula.js definiert
+
+
+
+
+   <script src="words_kids.js?v=7.0"></script>,
+
+    <script src="words_adult.js?v=7.0"></script>,
+    <script src="fragen/vocabulary.js?v=7.0"></script>,
+
+    <script src="fragen/questions_school.js?v=7.0"></script>,
+    <script src="fragen/questions_beruf.js?v=7.0"></script>,
+
+    <script src="fragen/questions_kfz.js?v=7.0"></script>,
+
+    <script src="fragen/questions_fun.js?v=7.0"></script>,
+    <script src="fragen/klasse1/questions_k1_mathe.js?v=7.0"></script>,
+    <script src="fragen/klasse1/questions_k1_deutsch.js?v=7.0"></script>,
+    <script src="fragen/klasse1/questions_k1_sachunterricht.js?v=7.0"></script>,
+
+    <script src="fragen/klasse2/questions_k2_mathe.js?v=7.0"></script>,
+    <script src="fragen/klasse2/questions_k2_deutsch.js?v=7.0"></script>,
+    <script src="fragen/klasse2/questions_k2_sachunterricht.js?v=7.0"></script>,
+    <script src="fragen/lasse2/questions_k2_englisch.js?v=7.0"></script>,
+
+    <script src="fragen/klasse3/questions_k3_mathe.js?v=7.0"></script>,
+    <script src="fragen/klasse3/questions_k3_deutsch.js?v=7.0"></script>,
+    <script src="fragen/klasse3/questions_k3_sachunterricht.js?v=7.0"></script>,
+    <script src="fragen/klasse3/questions_k3_englisch.js?v=7.0"></script>,
+
+    <script src="fragen/klasse4/questions_k4_mathe.js?v=7.0"></script>,
+    <script src="fragen/klasse4/questions_k4_deutsch.js?v=7.0"></script>,
+    <script src="fragen/klasse4/questions_k4_sachunterricht.js?v=7.0"></script>,
+    <script src="fragen/klasse4/questions_k4_englisch.js?v=7.0"></script>,
+
+    <script src="fragen/fun/kinder_anime.js?v=7.0"></script>,
+    <script src="fragen/fun/kinder_maerchen.js?v=7.0"></script>,
+    <script src="fragen/fun/kinder_tiere_natur.js?v=7.0"></script>,
+    <script src="fragen/fun/kinder_wissen.js?v=7.0"></script>,
+    <script src="fragen/fun/spass_allgemein.js"></script>,
+    <script src="fragen/fun/spass_erfindungen.js?v=7.0"></script>,
+    <script src="fragen/fun/spass_essen.js?v=7.0"></script>,
+    <script src="fragen/fun/spass_hauptstaedte.js?v=7.0"></script>,
+    <script src="fragen/fun/spass_hauptstaedte_europa.js?v=7.0"></script>,
+    <script src="fragen/fun/spass_musik.js?v=7.0"></script>,
+    <script src="fragen/fun/spass_nice_to_know.js?v=7.0"></script>,
+    <script src="fragen/fun/spass_raetsel.js?v=7.0"></script>,
+    <script src="fragen/fun/spass_schaetzfragen.js?v=7.0"></script>,
+    <script src="fragen/fun/spass_sport_film.js?v=7.0"></script>,
+    <script src="fragen/fun/spass_tiere.js?v=7.0"></script>,
+    <script src="fragen/fun/spass_ungewoehnliches.js?v=7.0"></script>,
+
+    <script src="formula.js?v=7.0"></script>
